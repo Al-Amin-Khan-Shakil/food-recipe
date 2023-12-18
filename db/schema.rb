d5d21be895f709733037dafff50e5fda48883a06
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-19 02:18:56 +0600

    Add test for user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_18_181043) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_18_192407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.time "preparation_time"
-    t.time "cooking_time"
+    t.string "preparation_time"
+    t.string "cooking_time"
     t.text "description"
     t.boolean "public"
     t.bigint "user_id", null: false
