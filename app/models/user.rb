9242cac66e79225ad321a92a07c7dc962fb454e4
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-21 03:28:30 +0600

    Style the recipe page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,60 +3,48 @@ class User < ApplicationRecord
   has_many :recipes
   has_many :foods
   has_many :recipe_foods, through: :recipes
-
   def general_shopping_list
-    required_food_quantities = calculate_required_food_quantities
+    required_food_quantitie = calculate_required_food_quantities
     user_food_quantities = calculate_user_food_quantities(required_food_quantities.keys)
-    missing_food_quantities = calculate_missing_food_quantities(required_food_quantities, user_food_quantities)
-
+    missing_food_quantitie = calculate_missing_food_quantities(required_food_quantities, user_food_quantities)
     {
-      missing_food_quantities:,
-      total_quantity: missing_food_quantities.values.sum { |info| info[:missing_quantity] },
-      total_cost: missing_food_quantities.values.sum { |info| info[:total_cost] },
-      total_food_items: missing_food_quantities.keys.size
+      missing_food_quantities:missing_food_quantitie,
+      total_quantity: missing_food_quantitie.values.sum { |info| info[:missing_quantity] },
+      total_cost: missing_food_quantitie.values.sum { |info| info[:total_cost] },
+      total_food_items: missing_food_quantitie.keys.size
     }
   end
-
   private
-
   def calculate_required_food_quantities
     recipes = Recipe.where(user: self)
     required_food_quantities = Hash.new(0)
-
     recipes.each do |recipe|
       recipe.recipe_foods.each do |recipe_food|
         required_food_quantities[recipe_food.food] += recipe_food.quantity
       end
     end
-
     required_food_quantities
   end
-
   def calculate_user_food_quantities(foods)
     user_food_quantities = Hash.new(0)
-
     recipe_foods.each do |user_recipe_food|
       if foods.include?(user_recipe_food.food)
         user_food_quantities[user_recipe_food.food] += user_recipe_food.food.quantity
       end
     end
-
     user_food_quantities
   end
-
   def calculate_missing_food_quantities(required_food_quantities, user_food_quantities)
     missing_food_quantities = {}
-
     required_food_quantities.each do |food, required_quantity|
       user_quantity = user_food_quantities[food]
-      missing_quantity = [required_quantity - user_quantity, 0].max
+      missing_quantit = [required_quantity - user_quantity, 0].max
       missing_food_quantities[food] = {
-        missing_quantity:,
-        total_cost: missing_quantity * food.price,
+        missing_quantity:missing_quantit,
+        total_cost: missing_quantit * food.price,
         food_name: food.name
       }
     end
-
     missing_food_quantities
   end
 end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
+  let(:user) { User.create(name: 'user1') }
+  let(:recipe) { user.recipes.create() }
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'should be response successull' do
+      get "/users/#{user.id}/posts"
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render the posts index file' do
+      get "/users/#{user.id}/posts"
+      expect(response).to render_template(:index)
+    end
+
+    it 'should include the placeholder' do
+      get "/users/#{user.id}/posts"
+      expect(response.body).to include('Pagination')
+    end
   end
 end
