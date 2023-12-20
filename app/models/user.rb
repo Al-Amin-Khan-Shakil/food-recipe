f45e98aac7edd6e3f816966524de049b55502059
Samuel wanza <s.wanza@alustudent.com>
2023-12-20 20:00:23 +0200

    reduced general shopping method length

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,43 +5,58 @@ class User < ApplicationRecord
   has_many :recipe_foods, through: :recipes
 
   def general_shopping_list
+    required_food_quantities = calculate_required_food_quantities
+    user_food_quantities = calculate_user_food_quantities(required_food_quantities.keys)
+    missing_food_quantities = calculate_missing_food_quantities(required_food_quantities, user_food_quantities)
+
+    {
+      missing_food_quantities:,
+      total_quantity: missing_food_quantities.values.sum { |info| info[:missing_quantity] },
+      total_cost: missing_food_quantities.values.sum { |info| info[:total_cost] },
+      total_food_items: missing_food_quantities.keys.size
+    }
+  end
+
+  private
+
+  def calculate_required_food_quantities
     recipes = Recipe.where(user: self)
     required_food_quantities = Hash.new(0)
 
-    # Calculate the total required quantity of each food in all recipes
     recipes.each do |recipe|
       recipe.recipe_foods.each do |recipe_food|
         required_food_quantities[recipe_food.food] += recipe_food.quantity
       end
     end
 
-    # Calculate the total quantity of each food the user has
+    required_food_quantities
+  end
+
+  def calculate_user_food_quantities(foods)
     user_food_quantities = Hash.new(0)
+
     recipe_foods.each do |user_recipe_food|
-      user_food_quantities[user_recipe_food.food] += user_recipe_food.food.quantity
+      if foods.include?(user_recipe_food.food)
+        user_food_quantities[user_recipe_food.food] += user_recipe_food.food.quantity
+      end
     end
 
-    # Calculate the missing quantities and total cost
+    user_food_quantities
+  end
+
+  def calculate_missing_food_quantities(required_food_quantities, user_food_quantities)
     missing_food_quantities = {}
-    total_cost = 0
 
     required_food_quantities.each do |food, required_quantity|
       user_quantity = user_food_quantities[food]
-      puts('user quantity:', required_quantity)
       missing_quantity = [required_quantity - user_quantity, 0].max
       missing_food_quantities[food] = {
         missing_quantity:,
         total_cost: missing_quantity * food.price,
         food_name: food.name
       }
-      total_cost += missing_food_quantities[food][:total_cost]
     end
 
-    {
-      missing_food_quantities:,
-      total_quantity: missing_food_quantities.values.sum { |info| info[:missing_quantity] },
-      total_cost:,
-      total_food_items: missing_food_quantities.keys.size
-    }
+    missing_food_quantities
   end
 end
