d5a304a1f3260ac97066729db2d9fd8c4db2e215
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-20 17:58:56 +0600

    Displayed recipe details

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,10 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes = current_user.recipes
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
+    @recipe = current_user.recipes.includes(recipe_foods: :food).find(params[:id])
   end
 
   def destroy
@@ -12,5 +12,11 @@ class RecipesController < ApplicationController
     @recipe.recipe_foods.destroy_all
     @recipe.destroy!
     redirect_to recipes_path, notice: 'Recipe successfully deleted.'
+  end
+
+  def toggle_public
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(public: !@recipe.public)
+    redirect_to @recipe, notice: 'Recipe public status toggled.'
   end
 end
